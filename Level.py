1aaaaaf8da81f20c9f51e75874a530b586d07861
ruslan <ruslan.emelianov@yahoo.com>
2021-01-08 02:17:07 +0200

    add mapgenerator

--- Level.py
+++ Level.py
@@ -22,29 +22,26 @@ class Level:
 		[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 	]
 
-	persist_group = pygame.sprite.Group()  # shouldn't be updated
-	dynamic_blocks_group = pygame.sprite.Group()  # should be updated
-	floor_blocks_group = pygame.sprite.Group()  # should be updated
 
 	def __init__(self):
+		self.generate_lvl()
+		self.persist_group = pygame.sprite.Group()  # shouldn't be updated
+		self.dynamic_blocks_group = pygame.sprite.Group()  # should be updated
+		self.floor_blocks_group = pygame.sprite.Group()  # should be updated
+		self.level = Level.level
+
+
+	def generate_lvl(self):
 		for i in range(len(self.level)):
 			for j in range(len(self.level)):
 				if self.level[i][j] == 0:
-					floor = Floor(i, j)
-					self.level[i][j] = Floor(i, j)
-					self.floor_blocks_group.add(floor)
+					self.floor_blocks_group.add(Floor(i, j))
 				if self.level[i][j] == 1:
-					wall = Wall(i, j)
-					self.level[i][j] = Wall(i, j)
-					self.persist_group.add(wall)
+					self.persist_group.add(Wall(i, j))
 				if self.level[i][j] == 2:
-					box = Box(i, j)
-					self.level[i][j] = Box(i, j)
-					self.dynamic_blocks_group.add(box)
+					self.dynamic_blocks_group.add(Box(i, j))
 				if self.level[i][j] == 4:
-					bomb = Bomb(i, j)
-					self.level[i][j] = Bomb(i, j)
-					self.dynamic_blocks_group.add(bomb)
+					self.dynamic_blocks_group.add(Bomb(i, j))
 
 	def get_persist_group(self):
 		print(self.persist_group)
@@ -55,6 +52,3 @@ class Level:
 
 	def get_floor_blocks_group(self):
 		return self.floor_blocks_group
-
-	def redraw_window(self):
-		pass
