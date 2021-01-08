1fba0b03a7a426e5fb44a267604fe3d61d9b7513
ruslan <ruslan.emelianov@yahoo.com>
2021-01-08 02:41:33 +0200

    add box random generator

--- mapgenerator/MapGenerator.py
+++ mapgenerator/MapGenerator.py
@@ -1,4 +1,5 @@
 import pprint
+import random
 
 level = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -20,7 +21,7 @@ level = [
 
 
 def generate_boxes(i, j):
-    list = [{
+    lists = [{
         "i": i - 1,
         "j": j - 1
     }, {
@@ -46,28 +47,26 @@ def generate_boxes(i, j):
         "j": j
     }]
 
-    # [i-1],[j-1]
-    # [i+1][j+1]
+    random_list = random.sample(range(0, 10), 8)
 
-    # [i+1][j-1]
-    # [i-1][j+1]
-    # [i][j-1]
-    # [i][j+1]
-    # [i-1][j]
-    # [i+1][j]
-
-# for i in range(len(level)):
-#     level[0][i] = 1  # up
-#     level[i][0] = 1  # left
-#     level[i][len(level)-1] = 1  # right
-#     level[len(level)-1][i] = 1  # down
-#     for j in range(len(level)):
-#         if 0 < i < len(level)-1:
-#             if 0 < j < len(level) - 1:
-#                 if i % 2 == 0 and j % 2 == 0:
-#                     level[i][j] = 1
-#                     generate_boxes(i,j)
+    magic_random_number = 3
+    for i in range(len(lists)):
+        if random_list[i] > magic_random_number:
+            level[lists[i]['i']][lists[i]['j']] = 4
 
 
-# pp = pprint.PrettyPrinter()
-# pp.pprint(level)
+for i in range(len(level)):
+    level[0][i] = 1  # up
+    level[i][0] = 1  # left
+    level[i][len(level)-1] = 1  # right
+    level[len(level)-1][i] = 1  # down
+    for j in range(len(level)):
+        if 0 < i < len(level)-1:
+            if 0 < j < len(level) - 1:
+                if i % 2 == 0 and j % 2 == 0:
+                    level[i][j] = 1
+                    generate_boxes(i,j)
+
+
+pp = pprint.PrettyPrinter()
+pp.pprint(level)
